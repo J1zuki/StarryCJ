cc6343df12aad3dadc7454173f6826d55a0d5664
J1zuki <joycekwek61@gmail.com>
2026-02-03 17:14:28 +0800

    updated login page by trying to add api and load into home page

--- js/login.js
+++ js/login.js
@@ -1,12 +1,34 @@
-document.getElementById("loginForm").addEventListener("submit", function (e) {
+document.getElementById("loginForm").addEventListener("submit", async function (e) {
   e.preventDefault();
 
-  const username = document.getElementById("username").value;
+  const email = document.getElementById("username").value;
   const password = document.getElementById("password").value;
 
-  if (username === "admin" && password === "1234") {
-    alert("Login successful ✨");
-  } else {
-    alert("Invalid username or password ❌");
+  try {
+    const response = await fetch("https://reqres.in/api/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        email: email,
+        password: password
+      })
+    });
+
+    const data = await response.json();
+
+    if (response.ok) {
+      // Save token from API
+      localStorage.setItem("token", data.token);
+
+      // Redirect
+      window.location.href = "home.html";
+    } else {
+      alert("Login failed.");
+    }
+
+  } catch (error) {
+    alert("Server error.");
   }
 });
