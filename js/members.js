92b6f22002338f4e3e655536305044981ddc1be9
Cylina Ho <hocylina@gmail.com>
2026-02-09 17:53:29 +0800

    change the images as i have imported the folder of avatar

--- js/members.js
+++ js/members.js
@@ -7,11 +7,11 @@
 ]
 
 const members = [
-    { id: 1, name: "Alan Yeo", avatar: "https://via.placeholder.com/60", group: "all" },
-    { id: 2, name: "Jemmy Lim", avatar: "https://via.placeholder.com/60", group: "all" },
-    { id: 3, name: "Gibil Teo", avatar: "https://via.placeholder.com/60", group: "all" },
-    { id: 4, name: "Amy Yeo", avatar: "https://via.placeholder.com/60", group: "all" },
-    { id: 5, name: "Elly Puah", avatar: "https://via.placeholder.com/60", group: "nearby" }
+    { id: 1, name: "Alan Yeo", avatar: "./images/members/alan.png", group: "all" },
+    { id: 2, name: "Jemmy Lim", avatar: "./images/members/jemmy.png", group: "all" },
+    { id: 3, name: "Gibil Teo", avatar: "./images/members/gibil.png", group: "all" },
+    { id: 4, name: "Amy Yeo", avatar: "./images/members/amy.png", group: "all" },
+    { id: 5, name: "Elly Puah", avatar: "./images/members/elly.png", group: "nearby" }
 ];
 
 function renderMembers() {
