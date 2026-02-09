1028b54d5fd3014949e17726d041dcdfb95cbc80
Cylina Ho <hocylina@gmail.com>
2026-02-09 17:46:03 +0800

    fixing the images

--- js/members.js
+++ js/members.js
@@ -1,9 +1,9 @@
 [
-  { "id": 1, "name": "Alan Yeo", "avatar": "./images/avatars/alan.png", "group": "all" },
-  { "id": 2, "name": "Jemmy Lim", "avatar": "./images/avatars/jemmy.png", "group": "all" },
-  { "id": 3, "name": "Gibil Teo", "avatar": "./images/avatars/gibil.png", "group": "all" },
-  { "id": 4, "name": "Amy Yeo", "avatar": "./images/avatars/amy.png", "group": "all" },
-  { "id": 5, "name": "Elly Puah", "avatar": "./images/avatars/elly.png", "group": "nearby" }
+  { "id": 1, "name": "Alan Yeo", "avatar": "./images/members/alan.png", "group": "all" },
+  { "id": 2, "name": "Jemmy Lim", "avatar": "./images/members/jemmy.png", "group": "all" },
+  { "id": 3, "name": "Gibil Teo", "avatar": "./images/members/gibil.png", "group": "all" },
+  { "id": 4, "name": "Amy Yeo", "avatar": "./images/members/amy.png", "group": "all" },
+  { "id": 5, "name": "Elly Puah", "avatar": "./images/members/elly.png", "group": "nearby" }
 ]
 
 const members = [
