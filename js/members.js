a55c1f5abfccd6411f27ef9255af819b0d7a32e0
Cylina Ho <hocylina@gmail.com>
2026-02-09 17:06:21 +0800

    create a member page for user to select and message them

---
+++ js/members.js
@@ -0,0 +1,7 @@
+[
+  { "id": 1, "name": "Alan Yeo", "avatar": "./images/avatars/alan.png", "group": "all" },
+  { "id": 2, "name": "Jemmy Lim", "avatar": "./images/avatars/jemmy.png", "group": "all" },
+  { "id": 3, "name": "Gibil Teo", "avatar": "./images/avatars/gibil.png", "group": "all" },
+  { "id": 4, "name": "Amy Yeo", "avatar": "./images/avatars/amy.png", "group": "all" },
+  { "id": 5, "name": "Elly Puah", "avatar": "./images/avatars/elly.png", "group": "nearby" }
+]
